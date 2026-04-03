8f4f18f112c5f3233dc7091447045d3632899d26
JasonBao <jasonbao@example.com>
2026-04-03 15:24:46 +0800

    [FIX] Solver evaluator - fixed evaluateCenters, added debug logging

--- src/solver.js
+++ src/solver.js
@@ -31,18 +31,27 @@ export class Solver {
   solve3x3(cube) {
     const solution = [];
     const testCube = cube.clone();
-    const maxIterations = 1000;
+    const maxIterations = 500;
     let iterations = 0;
+
+    console.log('[SOLVER] Starting solve, isSolved:', testCube.isSolved());
 
     while (!testCube.isSolved() && iterations < maxIterations) {
       const move = this.findBestMove(testCube);
       if (move) {
         testCube.move(move, true);
         solution.push(move);
+        if (iterations % 10 === 0) {
+          console.log('[SOLVER] Iteration', iterations, 'move:', move);
+        }
+      } else {
+        console.log('[SOLVER] No move found at iteration', iterations);
+        break;
       }
       iterations++;
     }
 
+    console.log('[SOLVER] Finished with', solution.length, 'moves');
     return this.optimizeSolution(solution);
   }
 
@@ -80,25 +89,23 @@ export class Solver {
 
   evaluateCube(cube) {
     let score = 0;
-
     score += this.evaluateCross(cube) * 10;
     score += this.evaluateF2L(cube) * 5;
     score += this.evaluateCenters(cube) * 3;
-
     return score;
   }
 
   evaluateCross(cube) {
     const crossEdges = [
-      { x: 1, y: 2, z: 2, faces: ['U', 'F'] },
-      { x: 2, y: 2, z: 1, faces: ['U', 'R'] },
-      { x: 1, y: 2, z: 0, faces: ['U', 'B'] },
-      { x: 0, y: 2, z: 1, faces: ['U', 'L'] }
+      { x: 1, y: 2, z: 2 },
+      { x: 2, y: 2, z: 1 },
+      { x: 1, y: 2, z: 0 },
+      { x: 0, y: 2, z: 1 }
     ];
 
     let solved = 0;
     for (const edge of crossEdges) {
-      const cubie = cube.getCubie(edge.x, edge.y, edge.z);
+      const cubie = cube.cubies.find(c => c.pos.x === edge.x && c.pos.y === edge.y && c.pos.z === edge.z);
       if (cubie && cubie.faceColors.py === 0xffffff && cubie.faceColors.pz === 0xff0000) {
         solved++;
       }
@@ -111,21 +118,19 @@ export class Solver {
   }
 
   evaluateCenters(cube) {
-    const centerColors = {
-      U: cube.getCubie(1, 2, 1)?.faceColors.py,
-      D: cube.getCubie(1, 0, 1)?.faceColors.ny,
-      F: cube.getCubie(1, 1, 2)?.faceColors.pz,
-      B: cube.getCubie(1, 1, 0)?.faceColors.nz,
-      L: cube.getCubie(0, 1, 1)?.faceColors.nx,
-      R: cube.getCubie(2, 1, 1)?.faceColors.px
-    };
+    const centers = cube.cubies.filter(c =>
+      c.pos.x === 1 && c.pos.y === 1 && c.pos.z === 1
+    );
+    if (centers.length === 0) return 0;
+    const center = centers[0];
 
     let matches = 0;
-    const expected = { U: 0xffffff, D: 0xffff00, F: 0xff0000, B: 0xff8800, L: 0x00ff00, R: 0x0000ff };
-
-    for (const face of Object.keys(expected)) {
-      if (centerColors[face] === expected[face]) matches++;
-    }
+    if (center.faceColors.py === 0xffffff) matches++;
+    if (center.faceColors.ny === 0xffff00) matches++;
+    if (center.faceColors.pz === 0xff0000) matches++;
+    if (center.faceColors.nz === 0xff8800) matches++;
+    if (center.faceColors.px === 0x0000ff) matches++;
+    if (center.faceColors.nx === 0x00ff00) matches++;
     return matches;
   }
 
@@ -147,12 +152,12 @@ export class Solver {
   }
 
   isFaceSolved(cube, face) {
-    const layer = face === 'U' ? 2 : face === 'D' ? 0 : 
+    const layer = face === 'U' ? 2 : face === 'D' ? 0 :
                   face === 'L' ? 0 : face === 'R' ? 2 :
                   face === 'F' ? 2 : 0;
     const axis = face === 'U' || face === 'D' ? 'y' : face === 'L' || face === 'R' ? 'x' : 'z';
     const cubies = cube.getCubiesInLayer(axis, layer);
-    
+
     const expectedColor = { U: 0xffffff, D: 0xffff00, F: 0xff0000, B: 0xff8800, L: 0x00ff00, R: 0x0000ff }[face];
     const faceKey = { U: 'py', D: 'ny', L: 'nx', R: 'px', F: 'pz', B: 'nz' }[face];
 
